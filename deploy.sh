7083db14567e90d833814016e231bc717fbae110
WeikangWeng <weikangweng@pop-os.localdomain>
2022-03-08 16:40:45 +0100

    small fix

--- deploy.sh
+++ deploy.sh
@@ -5,13 +5,13 @@ sudo apt-get -y update && sudo apt-get -y upgrade
 
 sudo apt-get install -y make pkg-config gcc build-essential
 
-cd Realloc_deployment/redis-6.2.6/deps/jemalloc/
+cd redis-6.2.6/deps/jemalloc/
 
 mv config.sh configure 
 
 cd -
 
-cd Realloc_deployment/redis-stock/deps/jemalloc/
+cd redis-stock/deps/jemalloc/
 
 mv config.sh configure 
 
