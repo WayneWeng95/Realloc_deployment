6ca7eb9b1b9ce4110040996b61a85d26d3ae0ada
WeikangWeng <weikangweng@pop-os.localdomain>
2022-03-08 16:39:16 +0100

    modify deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,3 +4,16 @@ sudo apt-get -y update && sudo apt-get -y upgrade
 
 
 sudo apt-get install -y make pkg-config gcc build-essential
+
+cd Realloc_deployment/redis-6.2.6/deps/jemalloc/
+
+mv config.sh configure 
+
+cd -
+
+cd Realloc_deployment/redis-stock/deps/jemalloc/
+
+mv config.sh configure 
+
+cd -
+
