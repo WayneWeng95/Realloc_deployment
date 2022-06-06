ed68f000ba4c8ef9d6c7cafc1dd8974d4662575a
WeikangWeng <weikangweng@pop-os.localdomain>
2022-06-06 14:19:30 +0200

    Temp Final

--- src/switchrealloc.h
+++ src/switchrealloc.h
@@ -1,11 +1,15 @@
 #define _GNU_SOURCE 1
 #include <stdio.h>
 #include <stdlib.h>
+#include <signal.h>
+#include <setjmp.h>
+#include <stdarg.h>
 #include <unistd.h>
 #include <string.h>
 #include <fcntl.h>
 #include <errno.h>
 #include <time.h>
+#include <math.h>
 #include <pthread.h>
 #include <threads.h>
 #include <malloc.h>
@@ -14,6 +18,7 @@
 #include <sys/stat.h>
 #include <sys/mman.h>
 #include <sys/types.h>
+#include <setjmp.h>
 
 #include "Config.h"
 
@@ -22,11 +27,23 @@
 #define MS 1000000
 #define KB 1024
 #define X2_EH_SIZE 131072
-//#define OFFSET sizeof(size_t) * 3
 #define SMALL_OFFSET 8
 #define OFFSET 24
 //#define PAGE_SIZE getpagesize()
 #define PAGE_SIZE 4096
+#define MID_PAGE_SIZE 16 * 1024
+#define HUGE_PAGE_SIZE 2 * 1024 * 1024 // 2048KB as system default
+
+
+#define __ALIGN_KERNEL(x, a) __ALIGN_KERNEL_MASK(x, (typeof(x))(a)-1)
+#define __ALIGN_KERNEL_MASK(x, mask) (((x) + (mask)) & ~(mask))
+#define ALIGN(x, a) __ALIGN_KERNEL((x), (a))
+#define PAGE_ALIGN(addr) ALIGN(addr, PAGE_SIZE)
+#define HUGE_PAGE_ALIGN(addr) ALIGN(addr, HUGE_PAGE_SIZE)
+#define MID_PAGE_ALIGN(addr) ALIGN(addr, MID_PAGE_SIZE)
+#define X2_ALIGN(addr) ALIGN(addr, X2_EH_SIZE)
+
+jmp_buf env;
 
 void *_malloc(size_t);
 
@@ -42,8 +59,4 @@ void *__go_to_size(void *);
 
 void *__go_to_count(void *);
 
-void *__go_to_head(void *);
-
 int *__assign_header(int *, int, int);
-
-int __create_fd(int);
